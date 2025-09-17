b70dd13c5d303a4a2dde8690bf58846e4f210323
GKanjiscuk <gi.kanjiscuk@gmail.com>
2025-09-16 22:58:31 -0300

    refactor[GEN-62]: Refatoração da Página de Kanban a fim de melhorar o desenvolvimento de recursos responsivos

--- taskmngr-frontend/src/components/BarraLateral.tsx
+++ taskmngr-frontend/src/components/BarraLateral.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default class BarraLateral extends React.Component {
     render() {
         return (
-            <div className="flex justify-between h-[calc(100vh-4.28rem)] w-14 bg-indigo-950 flex flex-col items-center py-8">
+            <div className="flex justify-between h-[calc(100vh-4.28rem)] w-14 bg-indigo-950 flex-col items-center py-8 flex-shrink-0">
                 <div className="flex flex-col gap-14">
                     <Link to="/home">
                         <i className="fa-solid fa-house text-2xl text-white cursor-pointer hover:text-indigo-200"></i>
