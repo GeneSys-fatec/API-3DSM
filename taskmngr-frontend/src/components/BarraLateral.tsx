adb6dd0f5239d09c168dc9294495fc9cb4685ab8
GKanjiscuk <gi.kanjiscuk@gmail.com>
2025-09-18 11:34:36 -0300

    fix: [GEN-62] Corrige exibição do menu hambúrguer e adiciona fechamento externo ao modal de projetos

--- taskmngr-frontend/src/components/BarraLateral.tsx
+++ taskmngr-frontend/src/components/BarraLateral.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-// O componente volta a ser de classe, como no seu original.
 export default class BarraLateral extends React.Component {
     render() {
         return (
             <>
-                {/* --- BARRA LATERAL PARA TELAS GRANDES (Desktop) --- */}
-                {/* A classe 'hidden lg:flex' faz com que ela só apareça em telas grandes */}
                 <div className="hidden lg:flex justify-between h-full w-16 border-t-3 border-white bg-indigo-950 flex-col items-center py-8 flex-shrink-0">
                     <div className="flex flex-col gap-14">
                         <Link to="/home" title="Início">
@@ -25,8 +22,6 @@ export default class BarraLateral extends React.Component {
                     </Link>
                 </div>
 
-                {/* --- BARRA DE NAVEGAÇÃO INFERIOR PARA TELAS PEQUENAS (Mobile) --- */}
-                {/* A classe 'lg:hidden' faz com que ela só apareça em telas pequenas */}
                 <div className="lg:hidden fixed bottom-0 left-0 w-full bg-indigo-950 shadow-lg z-50">
                     <div className="flex justify-around items-center h-16">
                         <Link to="/home" className="flex-1 flex justify-center p-2">
